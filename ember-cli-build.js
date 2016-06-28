440026f95d5e41f75d52cf7a6123ec6813634871
Will Duffy <willmanduffy@gmail.com>
2016-06-27 22:35:58 -0400

    Basic setup of Sass
    * Import Foundation
    * Setup ember-cli-sass and default to .sass extensions

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      extension: 'sass',
+
+      includePaths: [
+        'bower_components/foundation/scss'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
