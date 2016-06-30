528c093b225783d11cccb3407952e07683181c76
Will Duffy <willmanduffy@gmail.com>
2016-06-30 08:16:19 -0400

    Cleanup resume to compact timeline-panels and add images

--- app/components/timeline-panel.js
+++ app/components/timeline-panel.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNameBindings: ['inverted']
+  classNameBindings: ['inverted'],
+  inverted: Ember.computed.alias('experience.inverted')
 });
--- app/models/experience.js
+++ app/models/experience.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   // attributes
   dates: DS.attr('string'),
   description: DS.attr('string'),
+  image: DS.attr('string'),
   inverted: DS.attr('boolean'),
   name: DS.attr('string'),
   title: DS.attr('string'),
